22fc45e5f35a247b509a5852711059ea848e4dee
ArthurSalviano7 <arthursalviano7@gmail.com>
2025-09-06 11:50:05 -0300

    Estruturando repositório para atividades

---
+++ atividades/atividade_variaveis/questoes_variaveis.py
@@ -0,0 +1,149 @@
+# 1
+nome = "Arthur"
+print(f"Meu nome é {nome}")
+
+# 2
+nome = input("Insira seu nome: ")
+idade = input("insira sua idade: ")
+print(f"Olá {nome}, você tem {idade} anos")
+
+# 3
+cidade = "Campina Grande"
+uf = "PB"
+print(f"Moro em {cidade} - {uf}")
+
+# 4
+curso = input("Qual curso deseja fazer?")
+print(f"Você escolheu o curso de {curso}")
+
+# 5
+filme = "As aventuras de PI"
+ano = 2010
+print(f"O filme {filme} foi lançado em {ano}")
+
+# 6
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"A soma dos números é {num1 + num2}")
+
+# 7
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"A subtração dos números é {num1 - num2}")
+
+# 8
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"A multiplicação dos números é {num1 * num2}")
+
+# 9
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"A divisão dos números é {num1 / num2}")
+
+# 10
+num = int(input("Insira um número: "))
+print(f"O dobro do número é {num * 2}")
+
+# 11
+n1 = int(input("\nInsira uma nota: "))
+n2 = int(input("Insira outra nota: "))
+n3 = int(input("Insira outra nota: "))
+media = (n1 + n2 + n3)/3
+print(f"A média é {media:.2f}")
+
+# 12
+ano_atual = 2025
+ano_nascimento = 2003
+print(f"\nA idade calculada é {idade}")
+
+# 13
+altura = float(input("Insira sua altura: "))
+peso = int(input("Insira seu peso: "))
+imc = peso / (altura**2)
+print(f"\nO IMC calculado é {imc:.2f}")
+
+# 14
+dias = int(input("Informe a quantidade de dias: "))
+horas = dias * 24
+print(f"\n {dias} dias é equivalente a {horas} horas")
+
+# 15
+produto = float(input("Informe o valor do produto: "))
+desconto = produto / 10
+print(f"\nO produto com desconto vai para R$ {produto - desconto}")
+
+# 16
+num1 = int(input("\nInsira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"\nO maior número é {max(num1, num2)}")
+
+# 17
+num1 = int(input("\nInsira um número: "))
+num2 = int(input("Insira outro número: "))
+print(f"\nO menor número é {min(num1, num2)}")
+
+# 18
+num1 = int(input("\nInsira um número: "))
+num2 = int(input("Insira outro número: "))
+num3 = int(input("Insira outro número: "))
+print(f"\nA soma é {num1 + num2 + num3}")
+
+#19
+num1 = int(input("\nInsira um número: "))
+num2 = int(input("Insira outro número: "))
+num3 = int(input("Insira outro número: "))
+print(f"\nA média é {(num1 + num2 + num3) / 3}")
+
+# 20
+num = int(input("\nInsira um número: "))
+par_impar = "par" if num % 2 == 0 else "ímpar"
+print(par_impar) 
+
+# 21
+time = "Santos"
+jogador = "Neymar"
+print(f"{jogador} joga no time {time}")
+
+# 22
+livro = "'As Crônicas de Nárnia'"
+print(f"Estou lendo o livro {livro}")
+
+# 23
+profissao = "programador"
+empresa = "Google"
+print(f"\nTrabalho como {profissao} na {empresa}")
+
+# 24
+cor = input("Insira sua cor favorita: ")
+print(f"\nMinha cor favorita é {cor}.")
+
+# 25
+animal = "cachorro"
+nome = "Luck"
+print(f"\nMeu animal favorito é {animal} e se chama {nome}")
+
+# 26
+nome = input("qual seu nome? ")
+comida = input("qual sua comida favorita? ")
+print(f"\n {nome} gosta de comer {comida}")
+
+# 27
+cantor = "Teto"
+musica = "Paypal"
+print(f"\n{cantor} canta a música {musica}")
+
+# 28
+cidade = input("insira uma cidade: ")
+ano = input("Insira um ano: ")
+print(f"\nVisitei {cidade} em {ano}")
+
+# 29
+escola = input("Insira uma escola: ")
+curso = input("Insira um curso: ")
+print(f"\nEstudo {curso} na escola {escola}")
+
+# 30
+amigo = input("Insira um nome de um amigo: ")
+apelido = input("qual o apelido: ")
+print(f"\nMeu amigo {nome} também é conhecido como {apelido}")
