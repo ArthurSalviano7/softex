d7e31bd04c532bd213d4337678ccf2b21ca84594
ArthurSalviano7 <arthursalviano7@gmail.com>
2025-11-10 17:32:38 -0300

    Exercício POO atualizado

--- atividades/exercicios_python/classes.py
+++ atividades/exercicios_python/classes.py
@@ -1,3 +1,4 @@
+'''
 class Pessoa:
     nome = ""
     
@@ -85,4 +86,181 @@ produto2 = Produto("Notebook Lenovo", 1949.90)
 produto1.mostrarProduto()
 produto2.mostrarProduto()
 
+#7
+class Computador:
+    def __init__(self):
+        self.ligado = False
+    
+    def ligar(self):
+        self.ligado = True
+    
+    def mostrarComputador(self):
+        print("Computador está", "ligado" if self.ligado else "desligado")
+    
+computador = Computador()
+computador.mostrarComputador()
+computador.ligar()
+computador.mostrarComputador()    
 
+
+#7
+class Lampada:
+    def __init__(self):
+        self.acesa = False
+    
+    def ligar(self):
+        self.acesa = True
+    
+    def mostrarEstado(self):
+        print("Lâmpada está", "acesa" if self.acesa else "apagada")
+    
+lampada = Lampada()
+lampada.mostrarEstado()
+lampada.ligar()
+lampada.mostrarEstado()    
+
+class Funcionario:
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+    
+    def mostrarFuncionario(self):
+        print("Nome --> Salário")
+        print(self.nome, "-->", "R$", self.salario)
+        
+func1 = Funcionario("Joaquim da Silva", 2500)
+func2 = Funcionario("Alice Mendes", 1713)
+
+func1.mostrarFuncionario()
+func2.mostrarFuncionario()
+
+#12
+class Cachorro:
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def latir(self):
+        print("Au Au!")
+
+cachorro = Cachorro("Rex")
+cachorro.latir()
+
+#13
+class Celular:
+    def __init__(self, bateria):
+        self.bateria = bateria
+        
+    def carregar(self):
+        self.bateria += 1
+    
+    def completarCarga(self):
+        while self.bateria < 100:
+            self.carregar()
+            print("Carregando...", self.bateria, "%")
+
+celular = Celular(75)
+print("Carga atual: ", celular.bateria, "%")
+celular.completarCarga()
+
+#14
+class Livro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+    
+    def mostrarLivro(self):
+        print(self.titulo, "- escrito por", self.autor)
+    
+livro1 =  Livro("Contos de Terror", "Edgar Allan Poe")
+livro2 = Livro("Filhos de Húrin", "J.R.R Tolkien")
+
+livro1.mostrarLivro()
+livro2.mostrarLivro()
+
+#15
+class Aluno:
+    def __init__(self, nome, curso):
+        self.nome = nome
+        self.curso = curso
+    
+    def mostrarAluno(self):
+        print("Aluno", self.nome, "do Curso", self.curso)
+
+aluno1 = Aluno("Arthur Salviano", "Ciências da Computação")
+aluno2 = Aluno("Juliano Campos", "Engenharia Química")
+
+aluno1.mostrarAluno()
+aluno2.mostrarAluno()
+
+#16
+class Conta:
+    def __init__(self, nome):
+        self.nome = nome
+        self.saldo = 0
+    
+    def depositar(self, valor):
+        self.saldo += valor
+        print("Saldo atual: R$", self.saldo)
+    
+    def sacar(self, valor):
+        self.saldo -= valor
+        print("Saque de R$", valor, "efetuado!")
+        print("Saldo atual: R$", self.saldo)
+    
+    def mostrarConta(self):
+        print("Titular:", self.nome, "| Saldo: R$", self.saldo)
+
+conta = Conta("João Silva")
+conta.depositar(150.90)
+conta.mostrarConta()
+
+conta.depositar(float(input("Informe o valor do deposito:")))
+conta.mostrarConta()
+
+conta.sacar(float(input("Informe o valor para sacar: ")))
+
+#17
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def falar(self):
+        print("Olá, tudo bem?")
+    
+pessoa = Pessoa("João")
+pessoa.falar()
+
+#19
+class Carro:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+    
+    def mostrarCarro(self):
+        print("Carro da marca", self.marca, "| Modelo", self.modelo, "| Ano", self.ano)
+
+marca = input("Informe a marca do carro: ")
+modelo = input("Informe o modelo do carro: ")
+ano = int(input("Informe o ano do carro: "))
+
+carro = Carro(marca, modelo, ano)
+carro.mostrarCarro()
+
+#20
+class Retangulo:
+    def __init__(self, altura, largura):
+        self.altura = altura
+        self.largura = largura
+    
+    def area(self):
+        area = self.altura * self.largura
+        return area
+    
+    def mostrarRetangulo(self):
+        print("A área do seu retângulo é:", self.area())
+
+retangulo = Retangulo(10, 20)
+retangulo.mostrarRetangulo()
+'''
+#21
